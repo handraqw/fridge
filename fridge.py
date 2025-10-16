4d000c453535ee917f9bea017790865596ac1d8c
handraqw <boborakhimovmm@gmail.com>
2025-10-16 16:09:02 +0500

    Update fridge.py

--- fridge.py
+++ fridge.py
@@ -4,45 +4,28 @@ from decimal import Decimal
 goods = {
     'Пельмени Универсальные': [
         # Первая партия продукта 'Пельмени Универсальные':
-        {'amount': Decimal('0.5'), 'expiration_date': datetime.date(2025, 10, 15)},
+        {'amount': Decimal('0.5'), 'expiration_date': datetime.date(2023, 7, 15)},
         # Вторая партия продукта 'Пельмени Универсальные':
-        {'amount': Decimal('2'), 'expiration_date': datetime.date(2025, 11, 1)},
+        {'amount': Decimal('2'), 'expiration_date': datetime.date(2023, 8, 1)},
     ],
-
-    'Молоко': [
-        {'amount': Decimal('3'), 'expiration_date': datetime.date(2025, 11, 3)}
-    ],
-
     'Вода': [
         {'amount': Decimal('1.5'), 'expiration_date': None}
     ],
 }
 
-
-# def add(items, title, amount, expiration_date=None):
-#     ...
-
-
-# def add_by_note(items, note):
-#     ...
+def date_convert(expiration_date):
+    expiration_date = expiration_date.split('-')
+    return datetime.date(int(expiration_date[0]), int(expiration_date[1]), int(expiration_date[2]))
 
 
-# def find(items, needle):
-#     ...
+
+def add(items, title, amount, expiration_date=None):
+    if title not in items:
+        items[title] = [{'ammount': Decimal(amount), 'expiration_date': date_convert(expiration_date)}]
 
 
-# def amount(items, needle):
-#     ...
-
-# print(goods)
-# print()
-
-# goods['Мыло'] = Decimal('0.5')
-
-print(goods['Пельмени Универсальные'])
+print(goods)
 print()
-print(goods['Пельмени Универсальные'][0])
-print(goods['Пельмени Универсальные'][1]['amount'])
-
-if 'Молоко' in goods:
-    print('asd')
+add(goods, 'Cheese', '4', '2025-10-23')
+print()
+print(goods)
