591ababfbeddd746e44971534ccfb84043216893
handraqw <boborakhimovmm@gmail.com>
2025-10-16 15:22:59 +0500

    Update fridge.py

--- fridge.py
+++ fridge.py
@@ -19,17 +19,30 @@ goods = {
 }
 
 
-def add(items, title, amount, expiration_date=None):
-    ...
+# def add(items, title, amount, expiration_date=None):
+#     ...
 
 
-def add_by_note(items, note):
-    ...
+# def add_by_note(items, note):
+#     ...
 
 
-def find(items, needle):
-    ...
+# def find(items, needle):
+#     ...
 
 
-def amount(items, needle):
-    ...
+# def amount(items, needle):
+#     ...
+
+# print(goods)
+# print()
+
+# goods['Мыло'] = Decimal('0.5')
+
+print(goods['Пельмени Универсальные'])
+print()
+print(goods['Пельмени Универсальные'][0])
+print(goods['Пельмени Универсальные'][1]['amount'])
+
+if 'Молоко' in goods:
+    print('asd')
